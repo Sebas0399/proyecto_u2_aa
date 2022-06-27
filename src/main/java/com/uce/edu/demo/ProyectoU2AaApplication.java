5499b336449fff98e820d93741d74581183a9237
Sebas0399 <antony.aguay@gmail.com>
2022-06-26 21:02:22 -0500

    Log

--- src/main/java/com/uce/edu/demo/ProyectoU2AaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AaApplication.java
@@ -3,10 +3,10 @@ package com.uce.edu.demo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.apache.logging.log4j.*;
 @SpringBootApplication
 public class ProyectoU2AaApplication implements CommandLineRunner{
-
+	private static Logger log=LogManager.getLogger(ProyectoU2AaApplication.class.getName());
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2AaApplication.class, args);
 	}
@@ -14,7 +14,10 @@ public class ProyectoU2AaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hola munod");
+		log.info("Log exitoso");
+		log.error("Log error");
+		log.debug("Debug app");
+		//System.out.println("Hola munod");
 	}
 
 }
