95b8f7df72b6302aaa64d5adf4770c31bbf17118
Sebas0399 <antony.aguay@gmail.com>
2022-06-27 19:58:00 -0500

    crud

--- src/main/java/com/uce/edu/demo/ProyectoU2AaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AaApplication.java
@@ -1,23 +1,31 @@
 package com.uce.edu.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.logging.log4j.*;
+
+import com.uce.edu.demo.service.IPersonaJdbcService;
+import com.uce.edu.demo.to.Persona;
 @SpringBootApplication
 public class ProyectoU2AaApplication implements CommandLineRunner{
-	private static Logger log=LogManager.getLogger(ProyectoU2AaApplication.class.getName());
+	@Autowired
+	private IPersonaJdbcService personaService;
+	private static Logger LOG=LogManager.getLogger(ProyectoU2AaApplication.class.getName());
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2AaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		log.info("Log exitoso");
-		log.error("Log error");
-		log.debug("Debug app");
-		//System.out.println("Hola munod");
+		Persona persona=new Persona();
+		persona.setId(0);
+		persona.setNombre("Sebastian");
+		persona.setApellido("Arguello");
+//		this.personaService.actualizar(persona);
+		System.out.println(this.personaService.buscar(1));
 	}
 
 }
--- src/main/java/com/uce/edu/demo/service/PersonaJdbcServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/PersonaJdbcServiceImpl.java
@@ -1,8 +1,37 @@
 package com.uce.edu.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.uce.edu.demo.repository.IPersonaJdbcRepository;
+import com.uce.edu.demo.to.Persona;
+
 @Service
-public class PersonaJdbcServiceImpl {
+public class PersonaJdbcServiceImpl implements IPersonaJdbcService{
+	@Autowired
+	private IPersonaJdbcRepository personaRepo;
+	@Override
+	public Persona buscar(int id) {
+		// TODO Auto-generated method stub
+		return this.personaRepo.buscar(id);
+	}
+
+	@Override
+	public void guardar(Persona persona) {
+		// TODO Auto-generated method stub
+		this.personaRepo.insertar(persona);
+	}
+
+	@Override
+	public void actualizar(Persona persona) {
+		// TODO Auto-generated method stub
+		this.personaRepo.actualizar(persona);
+	}
+
+	@Override
+	public void eliminar(int id) {
+		// TODO Auto-generated method stub
+		this.personaRepo.eliminar(id);
+	}
 
 }
