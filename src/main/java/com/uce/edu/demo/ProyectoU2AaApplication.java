360400efd4d77ede2453ba099638af7ce2b4cd9a
Sebas0399 <antony.aguay@gmail.com>
2022-07-19 19:56:18 -0500

    relaciones entre objetos

--- src/main/java/com/uce/edu/demo/ProyectoU2AaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2AaApplication.java
@@ -1,5 +1,7 @@
 package com.uce.edu.demo;
 
+import java.math.BigDecimal;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,14 +9,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.demo.repository.modelo.Ciudadano;
+import com.uce.edu.demo.repository.modelo.Empleado;
 import com.uce.edu.demo.repository.modelo.PersonaSencilla;
-import com.uce.edu.demo.service.IPersonaJpaService;
+import com.uce.edu.demo.service.ICiudadanoService;
 
 @SpringBootApplication
 public class ProyectoU2AaApplication implements CommandLineRunner {
 	private static Logger LOG = LogManager.getLogger(ProyectoU2AaApplication.class.getName());
 	@Autowired
-	private IPersonaJpaService personaService;
+	private ICiudadanoService ciudadanoService;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2AaApplication.class, args);
 	}
@@ -24,7 +28,18 @@ public class ProyectoU2AaApplication implements CommandLineRunner {
 	
 		PersonaSencilla ps=new PersonaSencilla();
 		//LOG.info(this.personaService.buscarPorApellidoSencillo("Zambrano"));
-		LOG.info(this.personaService.contarGenero());
+		Ciudadano ciu1=new Ciudadano();
+		ciu1.setNombre("Jose");
+		ciu1.setApellido("Zambramo");
+		Empleado empl1=new Empleado();
+		empl1.setCodigoIess("578954");
+		empl1.setSalario(new BigDecimal(200));
+		empl1.setCiudadano(ciu1);
+		ciu1.setEmpleado(empl1);
+		this.ciudadanoService.insertar(ciu1);
+		
+		//insertar empleado xd
+		
 	}
 
 }
